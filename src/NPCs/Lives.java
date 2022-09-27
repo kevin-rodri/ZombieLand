a420b0b4c029d3d88999cfbe9b52676a548d8995
GandhiHarsh2003 <23gandhiharsh@gmail.com>
2022-09-27 19:31:58 -0400

    added new lives

--- src/NPCs/Lives.java
+++ src/NPCs/Lives.java
@@ -10,12 +10,21 @@ import GameObject.ImageEffect;
 import GameObject.SpriteSheet;
 import Level.NPC;
 import Level.Player;
+import Level.PlayerState;
 import Utils.Point;
 
 public class Lives extends NPC  {
 	public Lives(int id, Point location) {
         super(id, location.x, location.y, new SpriteSheet(ImageLoader.load("Heart.png"), 13, 13), "STAND_LEFT");
     }
+	
+	 public void update(Player player) {
+	        if (player.overlaps(this) && player.getPlayerState() == PlayerState.WALKING) {
+	            this.setIsHidden(true);
+
+	        }
+	        super.update(player);
+	 }
 
     @Override
     public HashMap<String, Frame[]> loadAnimations(SpriteSheet spriteSheet) {
