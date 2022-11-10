827c174f87753f1c775fdeb08604d82b1be367f1
Brian Wanamaker <bcwanamaker@quinnipiac.edu>
2022-11-09 23:15:36 -0500

    made shop not passable again and updated start color

--- src/Utils/Colors.java
+++ src/Utils/Colors.java
@@ -5,7 +5,7 @@ import java.awt.*;
 // just defines some colors based on rgb that are used in the game
 public class Colors {
     public static final Color MAGENTA = new Color(255, 0, 255);
-    public static final Color CORNFLOWER_BLUE = new Color(100, 149, 237);
+    public static final Color CORNFLOWER_BLUE = new Color(40,40,40);
 
     private Colors() { }
 }
