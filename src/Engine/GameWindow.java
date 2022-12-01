1901df4dd51353bee67fbd57e48cf62c2753789a
Aaryan Kapoor <aaryankapoor2003@gmail.com>
2022-11-30 19:58:15 -0500

    change game window name

--- src/Engine/GameWindow.java
+++ src/Engine/GameWindow.java
@@ -15,7 +15,7 @@ public class GameWindow {
 	private GridLayout layout;
 
 	public GameWindow() {
-		gameWindow = new JFrame("Game");
+		gameWindow = new JFrame("Project Z");
 		gamePanel = new GamePanel();
 		gameWindow.setLayout(new GridLayout(0,1));
 	//	gamePanel2 = new GamePanel();
